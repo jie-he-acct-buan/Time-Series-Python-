fbb99bd8664fba658ba2da5c950ccdccd409828a
jie-he-acct-buan <49351579+jie-he-acct-buan@users.noreply.github.com>
2019-04-06 16:31:27 -0500

    Add files via upload

---
+++ introduction to times series analysis.py
@@ -0,0 +1,498 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 18:44:00 2019
+
+@author: Jie
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from pandas import Series
+
+# Import pandas and plotting modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Convert the date index to datetime
+diet.index = pd.to_datetime(diet.index)
+
+# Plot 2012 data using slicing
+diet['2012'].plot()
+plt.show()
+
+# Plot the entire time series diet and show gridlines
+diet.plot()
+plt.show()
+
+#############################################################################
+# Import pandas
+import pandas as pd
+
+# Convert the stock index and bond index into sets
+set_stock_dates = set(stocks.index)
+set_bond_dates = set(bonds.index)
+
+# Take the difference between the sets and print
+print(set_stock_dates - set_bond_dates)
+
+# Merge stocks and bonds DataFrames using join()
+stocks_and_bonds = stocks.join(bonds,how='inner')
+
+
+###############################################################################
+# Compute percent change using pct_change()
+returns = stocks_and_bonds.pct_change()
+
+# Compute correlation using corr()
+correlation = returns['SP500'].corr(returns['US10Y'])
+print("Correlation of stocks and interest rates: ", correlation)
+
+# Make scatter plot
+plt.scatter(returns['SP500'],returns['US10Y'])
+plt.show()
+##############################################################################
+UFO=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/UFO.csv')
+DJI=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/DJI.csv')
+UFO=UFO.set_index('Date')
+DJI=DJI.set_index('Date')
+levels=UFO.join(DJI, lsuffix='_left', rsuffix='_right', how='inner')
+levels=levels.rename(columns={'Value_left':'UFO','Value_right':'DJI'})
+
+# Compute correlation of levels
+correlation1 = levels['DJI'].corr(levels['UFO'])
+print("Correlation of levels: ", correlation1)
+changes = levels.pct_change
+
+# Compute correlation of percent changes
+changes = levels.pct_change()
+correlation2 = changes['DJI'].corr(changes['UFO'])
+print("Correlation of changes: ", correlation2)
+
+##############################################################################
+# Import the statsmodels module
+import statsmodels.api as sm
+
+# Compute correlation of x and y
+correlation = x.corr(y)
+print("The correlation between x and y is %4.2f" %(correlation))
+
+# Convert the Series x to a DataFrame and name the column x
+x = pd.DataFrame(x, columns=['x'])
+
+# Add a constant to the DataFrame x
+x = sm.add_constant(x)
+
+# Fit the regression of y on x
+result = sm.OLS(y,x).fit()
+
+# Print out the results and look at the relationship between R-squared and the correlation above
+print(result.summary())
+###############################################################################
+MSFT=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/MSFT.csv')
+MSFT=MSFT.set_index(['Date'])
+MSFT.index=pd.to_datetime(MSFT.index)
+# Convert the daily data to weekly data
+MSFT = MSFT.resample('W').last()
+
+# Compute the percentage change of prices
+returns = MSFT.pct_change()
+
+# Compute and print the autocorrelation of returns
+autocorrelation = returns['Adj Close'].autocorr()
+print("The autocorrelation of weekly returns is %4.2f" %(autocorrelation))
+###############################################################################
+# Compute the daily change in interest rates 
+daily_data['change_rates'] = daily_data.diff()
+
+# Compute and print the autocorrelation of daily changes
+autocorrelation_daily = daily_data['change_rates'].autocorr()
+
+print("The autocorrelation of daily interest rate changes is %4.2f" %(autocorrelation_daily))
+
+# Convert the daily data to annual data
+annual_data = daily_data['US10Y'].resample('A').last()
+
+# Repeat above for annual data
+annual_data['diff_rates'] = annual_data.diff()
+autocorrelation_annual = annual_data['diff_rates'].autocorr()
+print("The autocorrelation of annual interest rate changes is %4.2f" %(autocorrelation_annual))
+###################################################################################
+HRB=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/HRB.csv')
+HRB=HRB.set_index(['Quarter'])
+HRB.index=pd.to_datetime(HRB.index)
+
+# Import the acf module and the plot_acf module from statsmodels
+from statsmodels.tsa.stattools import acf
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Compute the acf array of HRB
+acf_array = acf(HRB)
+print(acf_array)
+
+# Plot the acf function
+plot_acf(HRB,alpha=1)
+plt.show()
+###############################################################################
+MSFT=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/MSFT.csv')
+MSFT=MSFT.set_index(['Date'])
+MSFT.index=pd.to_datetime(MSFT.index)
+# Convert the daily data to weekly data
+MSFT = MSFT.resample('W').last()
+returns=MSFT.pct_change()[1:]
+
+# Import the plot_acf module from statsmodels and sqrt from math
+from statsmodels.tsa.stattools import acf
+from statsmodels.graphics.tsaplots import plot_acf
+from math import sqrt
+
+# Compute and print the autocorrelation of MSFT weekly returns
+autocorrelation = returns['Adj Close'].autocorr()
+print("The autocorrelation of weekly MSFT returns is %4.2f" %(autocorrelation))
+
+# Find the number of observations by taking the length of the returns DataFrame
+nobs = len(returns)
+
+# Compute the approximate confidence interval
+conf = 1.96/sqrt(nobs)
+print("The approximate confidence interval is +/- %4.2f" %(conf))
+
+# Plot the autocorrelation function with 95% confidence intervals and 20 lags using plot_acf
+
+plt.clf()
+plot_acf(returns[1:], alpha=0.05, lags=20)
+plt.show()
+###############################################################################
+# Import the plot_acf module from statsmodels
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Simulate white noise returns
+returns = np.random.normal(loc=0.02, scale=0.05, size=1000)
+
+# Print out the mean and standard deviation of returns
+mean = np.mean(returns)
+std = np.std(returns)
+print("The mean is %5.3f and the standard deviation is %5.3f" %(mean,std))
+
+# Plot returns series
+plt.plot(returns)
+plt.show()
+
+# Plot autocorrelation function of white noise returns
+plot_acf(returns,lags=20)
+plt.show()
+####################################################################################
+# Generate 500 random steps with mean=0 and standard deviation=1
+steps = np.random.normal(loc=0, scale=1, size=500)
+
+# Set first element to 0 so that the first price will be the starting stock price
+steps[0]=0
+
+# Simulate stock prices, P with a starting price of 100
+P = 100 + np.cumsum(steps)
+
+# Plot the simulated stock prices
+plt.plot(P)
+plt.title("Simulated Random Walk")
+plt.show()
+###############################################################################
+
+# Generate 500 random steps
+steps = np.random.normal(loc=0.001, scale=0.01, size=500) + 1
+
+# Set first element to 1
+steps[0]=1
+
+# Simulate the stock price, P, by taking the cumulative product
+P = 100 * np.cumprod(steps)
+
+# Plot the simulated stock prices
+plt.plot(P)
+plt.title("Simulated Random Walk with Drift")
+plt.show()
+
+###############################################################################
+AMZN=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/AMZN.csv')
+AMZN=AMZN.set_index(['Date'])
+AMZN.index=pd.to_datetime(AMZN.index)
+# Import the adfuller module from statsmodels
+from statsmodels.tsa.stattools import adfuller
+
+# Run the ADF test on the price series and print out the results
+results = adfuller(AMZN['Adj Close'])
+print(results)
+
+# Just print out the p-value
+print('The p-value of the test on prices is: ' + str(results[1]))
+
+################################################################################
+# Import the adfuller module from statsmodels
+from statsmodels.tsa.stattools import adfuller
+
+# Create a DataFrame of AMZN returns
+AMZN_ret = AMZN.pct_change()[1:]
+# Eliminate the NaN in the first row of returns
+
+
+# Run the ADF test on the return series and print out the p-value
+results = adfuller(AMZN_ret['Adj Close'])
+plt.plot(AMZN_ret)
+print('results for AD Fuller tests: \n', results)
+print('The p-value of the test on returns is: ' + str(results[1]))
+
+################################################################################
+HRB=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/HRB.csv')
+HRB=HRB.set_index(['Quarter'])
+HRB.index=pd.to_datetime(HRB.index)
+# Import the acf module and the plot_acf module from statsmodels
+from statsmodels.tsa.stattools import acf
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Plot the acf function
+plot_acf(HRB,alpha=1)
+plt.show()
+
+# Seasonally adjust quarterly earnings
+HRBsa = HRB.diff(4)[4:]
+
+# Plot the autocorrelation function of the seasonally adjusted series
+plot_acf(HRBsa)
+plt.show()
+################################################################################
+# import the module for simulating data
+from statsmodels.tsa.arima_process import ArmaProcess
+plt.clf()
+# Plot 1: AR parameter = +0.9
+plt.subplot(2,1,1)
+ar1 = np.array([1, -0.9])
+ma1 = np.array([1])
+AR_object1 = ArmaProcess(ar1, ma1)
+simulated_data_1 = AR_object1.generate_sample(nsample=1000)
+plt.plot(simulated_data_1)
+
+# Plot 2: AR parameter = -0.9
+plt.subplot(2,1,2)
+ar2 = np.array([1, 0.9])
+ma2 = np.array([1])
+AR_object2 = ArmaProcess(ar2, ma2)
+simulated_data_2 = AR_object2.generate_sample(nsample=1000)
+plt.plot(simulated_data_2)
+plt.show()
+###############################################################################
+# import the module for simulating data
+from statsmodels.tsa.arima_process import ArmaProcess
+plt.clf()
+# Plot 1: AR parameter = +0.9
+ar1 = np.array([1, -0.9])
+ma1 = np.array([1])
+AR_object1 = ArmaProcess(ar1, ma1)
+simulated_data_1 = AR_object1.generate_sample(nsample=1000)
+plot_acf(simulated_data_1, alpha=1, lags=20)
+plt.show()
+
+# Plot 2: AR parameter = -0.5
+ar2 = np.array([1, 0.5])
+ma2 = np.array([1])
+AR_object2 = ArmaProcess(ar2, ma2)
+simulated_data_2 = AR_object2.generate_sample(nsample=1000)
+plot_acf(simulated_data_2, alpha=1, lags=20)
+plt.show()
+###############################################################################
+# import the module for simulating data
+from statsmodels.tsa.arima_process import ArmaProcess
+plt.clf()
+# Plot 1: AR parameter = +0.9
+ar1 = np.array([1, -0.9])
+ma1 = np.array([1])
+AR_object1 = ArmaProcess(ar1, ma1)
+simulated_data_1 = AR_object1.generate_sample(nsample=1000)
+
+# Import the ARMA module from statsmodels
+from statsmodels.tsa.arima_model import ARMA
+
+# Fit an AR(1) model to the first simulated data
+mod = ARMA(simulated_data_1, order=(1,0))
+res = mod.fit()
+
+# Print out summary information on the fit
+print(res.summary())
+
+# Print out the estimate for the constant and for phi
+print("When the true phi=0.9, the estimate of phi (and the constant) are:")
+print(res.params[1])
+
+# Forecast the first AR(1) model
+res.plot_predict(start=990, end=1010)
+plt.show()
+###############################################################################
+# import the module for simulating data
+from statsmodels.tsa.arima_process import ArmaProcess
+
+# Plot 1: MA parameter = -0.9
+plt.subplot(2,1,1)
+ar1 = np.array([1])
+ma1 = np.array([1, -0.9])
+MA_object1 = ArmaProcess(ar1, ma1)
+simulated_data_1 = MA_object1.generate_sample(nsample=1000)
+plt.plot(simulated_data_1)
+
+# Plot 2: MA parameter = +0.9
+plt.subplot(2,1,2)
+ar2 = np.array([1])
+ma2 = np.array([1, 0.9])
+MA_object2 = ArmaProcess(ar2, ma2)
+simulated_data_2 = MA_object2.generate_sample(nsample=1000)
+plt.plot(simulated_data_2)
+
+plt.show()
+###############################################################################
+intraday=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/Sprint_Intraday.txt',header=None)
+intraday=intraday.iloc[:,[0,1]]
+
+# import datetime module
+import datetime
+# Change the first date to zero
+intraday.iloc[0,0] = 0
+# Change the column headers to 'DATE' and 'CLOSE'
+intraday=intraday.rename(columns={0:'DATE',1:'CLOSE'})
+# Examine the data types for each column
+print(intraday.dtypes)
+# Convert DATE column to numeric
+intraday['DATE'] = pd.to_numeric(intraday['DATE'])
+# Make the `DATE` column the new index
+intraday = intraday.set_index('DATE')
+# Notice that some rows are missing
+print("The length of the DataFrame is: ",len(intraday))
+# Find the missing rows
+print("Missing rows: ", set(range(391)) - set(intraday.index))
+# Fill in the missing rows
+intraday = intraday.reindex(range(391), method='ffill')
+# Change the index to the intraday times
+intraday.index = pd.date_range(start='2017-08-28 9:30', end='2017-08-28 16:00', freq='1min')
+# Plot the intraday time series
+intraday.plot(grid=True)
+plt.show()
+
+###############################################################################
+# Import plot_acf and ARMA modules from statsmodels
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.arima_model import ARMA
+
+# Compute returns from prices and drop the NaN
+returns = intraday.pct_change()
+returns = returns.dropna()
+
+# Plot ACF of returns with lags up to 60 minutes
+plot_acf(returns, lags=60)
+plt.show()
+# Fit the data to an MA(1) model
+mod = ARMA(returns, order=(0,1))
+res = mod.fit()
+print(res.params)
+##############################################################################
+# import the modules for simulating data and plotting the ACF
+from statsmodels.tsa.arima_process import ArmaProcess
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Build a list MA parameters
+ma = [0.8**i for i in range(30)]
+
+# Simulate the MA(30) model
+ar = np.array([1])
+AR_object = ArmaProcess(ar, ma)
+simulated_data = AR_object.generate_sample(nsample=5000)
+
+# Plot the ACF
+plot_acf(simulated_data, lags=30)
+plt.show()
+#################################################################################
+HO=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/CME_HO1.csv')
+HO=HO.set_index(['Date'])
+
+NG=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/CME_NG1.csv')
+NG=NG.set_index(['Date'])
+
+# Plot the prices separately
+plt.subplot(2,1,1)
+plt.plot(7.25*HO, label='Heating Oil')
+plt.plot(NG, label='Natural Gas')
+plt.legend(loc='best', fontsize='small')
+
+# Plot the spread
+plt.subplot(2,1,2)
+plt.plot(7.25*HO-NG, label='Spread')
+plt.legend(loc='best', fontsize='small')
+plt.axhline(y=0, linestyle='--', color='k')
+plt.show()
+###################################################################################
+temp_NY=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Introduction to Time Series/NOAA_TAVG.csv')
+temp_NY=temp_NY.set_index(['DATE'])
+
+# Import the adfuller function from the statsmodels module
+from statsmodels.tsa.stattools import adfuller
+
+# Convert the index to a datetime object
+temp_NY.index = pd.to_datetime(temp_NY.index, format='%Y')
+
+# Plot average temperatures
+temp_NY.mean()
+temp_NY.plot()
+plt.show()
+
+# Compute and print ADF p-value
+result = adfuller(temp_NY['TAVG'])
+print("The p-value for the ADF test is ", result[1])
+
+# Import the modules for plotting the sample ACF and PACF
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+# Take first difference of the temperature Series
+chg_temp = temp_NY.diff()
+chg_temp = chg_temp.dropna()
+
+# Plot the ACF and PACF on the same page
+fig, axes = plt.subplots(2,1)
+
+# Plot the ACF
+plot_acf(chg_temp, lags=20, ax=axes[0])
+
+# Plot the PACF
+plot_pacf(chg_temp, lags=20, ax=axes[1])
+plt.show()
+
+# Import the module for estimating an ARMA model
+from statsmodels.tsa.arima_model import ARMA
+
+# Fit the data to an AR(1) model and print AIC:
+mod = ARMA(chg_temp, order=(1,0))
+res = mod.fit()
+print("The AIC for an AR(1) is: ", res.aic)
+
+# Fit the data to an AR(2) model and print AIC:
+mod = ARMA(chg_temp, order=(2,0))
+res = mod.fit()
+print("The AIC for an AR(2) is: ", res.aic)
+
+# Fit the data to an MA(1) model and print AIC:
+mod = ARMA(chg_temp, order=(0,1))
+res = mod.fit()
+print("The AIC for an MA(1) is: ", res.aic)
+
+# Fit the data to an ARMA(1,1) model and print AIC:
+mod = ARMA(chg_temp, order=(1,1))
+res = mod.fit()
+print("The AIC for an ARMA(1,1) is: ", res.aic)
+
+# Import the ARIMA module from statsmodels
+from statsmodels.tsa.arima_model import ARIMA
+
+# Forecast interest rates using an AR(1) model
+mod = ARIMA(temp_NY, order=(1,1,1))
+res = mod.fit()
+
+# Plot the original series and the forecasted series
+res.plot_predict(start='1872-01-01', end='2046-01-01')
+plt.show()
+
+
